f32f733f6c4743d358bd7d358cc3fd4da8b15089
ANT <velmyshanovnyi@gmail.com>
2019-08-02 12:40:19 +0300

    Update ptrender.cmd

--- ptrender.cmd
+++ ptrender.cmd
@@ -10,6 +10,16 @@ setlocal enableextensions enabledelayedexpansion
 @echo.
 @echo.
 
+rem ПОТРІБНО --------------------------------------------
+rem щоб вже були заінсталені:
+rem 1) PTgui   - https://github.com/velmyshanovnyi/ptrender/wiki/PTgui-install
+rem 2) NodeJS  - взяти тут: https://nodejs.org 
+rem NodeJS: - після власне своєї інсталяції, NodeJS інсталить ще для всоєї роботи наспупне:
+rem NodeJS: - Chocolatey 
+rem NodeJS: - Python 2.7
+rem NodeJS: - Microsoft VisualStudio2017Buildtools
+rem NodeJS: - і ще деякі бібліотеки...
+
 
 @set ptrenderGitPath=D:\My_git\ptrender
 @set ptrenderSrcPath=G:\ptrender
@@ -41,4 +51,5 @@ node render.js %ptrenderRenderPath%
 @echo.
 @echo.
 @echo.
+
 cmd /k
